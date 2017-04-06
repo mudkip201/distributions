b803d2f70943b31067d9e6ecacac25d646523b1b
Matthew Cowen-Green <mudkipcg@yahoo.om>
2017-04-06 19:32:50 -0400

    updates

---
+++ listofdistributions.py
@@ -0,0 +1,294 @@
+'''
+alpha
+amoroso
+anglit
+arcsine
+arctangent
+asymmetric laplace
+balding-nichols
+banach
+bartlett
+bates
+behrens-fisher
+benford
+benini
+benktander weibull
+bernoulli
+beta
+betabinomial
+beta-exponential
+beta-exponential burr XII
+beta-gompertz
+beta-pascal
+beta prime
+bhattacharya negative binomial
+binomial
+birnbaum-saunders
+bounded pareto
+bradford
+bramwell-holdsworth-pinton
+burr II
+burr III
+burr V
+burr VI
+burr VII
+burr VIII
+burr X
+burr XII
+cauchy
+chi
+chi square
+chi square exponential
+complementary exponentiated exponential-geometric lifetime
+complementary exponentiated inverted weibull-logarithmic (logistic?)
+complementary exponentiated inverted weibull-poisson
+compound poisson
+cosine
+dagum
+discrete weibull
+double weibull
+erlang
+exponentially-modified normal
+exponential
+exponential geometric
+exponential normal
+exponentiated discrete weibull
+exponentiated exponential binomial
+exponentiated exponential-geometric
+exponentiated frechet
+exponentiated generalized extreme value
+exponentiated generalized frechet
+exponentiated generalized gumbel
+exponentiated generalized inverse exponential
+exponentiated generalized inverse weibull
+exponentiated generalized linear exponential
+exponentiated generalized normal
+exponentiated gompertz
+exponentiated gumbel
+exponentiated kumaraswamy
+exponentiated kumaraswamy-dagum
+exponentiated logistic (logarithmic?)
+exponentiated lomax
+exponentiated lomax-poisson
+exponentiated modified weibull extension
+exponentiated transmuted generalized rayleigh
+exponentiated transmuted weibull
+exponentiated weibull
+exponentiated weibull-geometric
+exponentiated weibull-logarithmic (logistic?)
+exponentiated weibull-poisson
+extended generalized exponential
+extreme value
+extreme value max
+extreme value min
+f
+feller-pareto
+fisher-z
+fisk
+folded normal
+frechet
+friemer, mudholkar, kollia, and lin generalized tukey-lambda
+gamma
+gamma-exponential
+gamma shifted gompertz
+g and h
+gauss hyper
+gaussian kuzmin
+generalized beta prime
+generalized exponential
+generalized exponentiated poisson
+generalized extreme value
+generalized gamma
+generalized gompertz
+generalized gumbel
+generalized inverse generalized exponential
+generalized inverse weibull
+generalized lambda
+generalized lindley
+generalized linear failure rate-geometric
+generalized logistic
+generalized logistic V
+generalized odd log-logistic
+generalized pareto
+generalized pearson VII
+generalized topp-leone
+generalized tukey-lambda
+generalized type I extreme value
+generalized weibull-exponential
+geometric
+geometric extreme-exponential
+gompertz
+gompertz-makeham
+gumbel
+gumbel2
+half-cauchy
+half-generalized pearson VII
+half-laha
+half-logistic
+half-normal
+harris extended exponential
+hermite
+hyperbolic secant
+hyperexponential
+hypergeometric
+hypoexponential
+inverse chi square
+inverse exponential
+inverse gamma
+inverse normal
+inverse paralogistic
+inverse weibull
+irwin-hall
+johnson sb
+johnson sl
+johnson su
+k
+kappa
+kolmogorov-smirnov one
+kolmogorov-smirnov two large
+kumaraswamy
+kumaraswamy 4
+kumaraswamy dagum
+kumaraswamy exponentiated pareto
+kumaraswamy flexible weibull extension
+kumaraswamy generalized exponentiated exponential
+kumaraswamy generalized exponentiated pareto
+kumaraswamy generalized exponentiated weibull
+kumaraswamy generalized half-normal
+kumaraswamy generalized power-weibull
+kumaraswamy gumbel
+kumaraswamy half-cauchy
+kumaraswamy kumaraswamy
+kumaraswamy linear exponential
+kumaraswamy log-logistic
+kumaraswamy pareto
+laha
+laplace
+levy
+levy l
+levy stable
+log cauchy
+log gamma
+log generalized lindley weibull
+logistic
+logistic burr XII
+logistic exponential
+logistic frechet
+logistic log-logistic
+logistic lomax
+logistic pareto
+logistic rayleigh
+logistic uniform
+logistic weibull
+log-laplace
+log-logistic
+log-normal
+log-triangle
+lomax
+maxwell-boltzmann
+mcdonald log-logistic
+meridian
+mielke beta-kappa
+minimax
+modified extended generalized exponential
+moffat
+moyal
+multinomial
+nakagami
+negative binomial
+negative hypergeometric
+negative multinomial
+non-central beta
+non-central chi square
+non-central f
+non-central t
+normal
+normal-gamma
+normal-inverse gamma
+odd generalized exponentiated gompertz
+odd generalized exponentiated linear failure rate
+odd generalized exponentiated log-logistic
+odds generalized exponentiated exponential
+paralogistic
+pareto
+pareto 2
+pareto 3
+pareto 4
+pearson III
+pearson VII
+poisson
+poisson exponential
+polya
+positive negative binomial
+power
+power-log normal
+power-normal
+prentice
+q-exponential
+q-gaussian
+rademacher
+raised cosine
+rayleigh
+r
+reciprocal
+reciprocal inverse gaussian
+rectified normal
+reflected power
+reflected topp-leone
+reversed burr XII
+rice
+scaled chi square
+scaled inverse chi square
+shifted gompertz
+shifted log-logistic
+skellam
+skew laplace
+skew logistic
+skew normal
+slash
+slope
+standard beta prime
+standard prentice
+suzuki
+symmetric prentice
+t
+topp-leone
+transmuted complementary weibull-geometric
+transmuted exponentiated exponential
+transmuted exponentiated frechet
+transmuted exponentiated lomax
+transmuted generalized linear failure rate
+transmuted generalized rayleigh
+transmuted inverse exponential
+transmuted kumaraswamy
+transmuted modified inverse rayleigh
+transmuted weibull-lomax
+triangular
+truncated exponential
+truncated normal (both sides)
+truncated normal (left side)
+truncated normal (right side)
+tsallis-q
+tukey-lambda
+two-sided power
+uniform
+uniform-product
+unit gamma
+u-power
+u-quad
+voigt
+wakeby
+wedge
+weibull
+weibull-burr XII
+weibull-frechet
+weibull-generalized exponential
+weibull-lomax
+weibull-weibull
+wein ?
+xgamma
+yule-simon
+zero-inflated negative binomial-generalized exponential
+zero-truncated poisson
+zipf
+'''
